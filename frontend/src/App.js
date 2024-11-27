ae14e11ce803d2031060a05e216f241ed57e40c1
RayenMiri <rayenmiri000@gmail.com>
2024-11-27 21:30:50 +0100

    setting up the frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "./Auth/sign_in";
+import SignUp from "./Auth/sign_up";
+
+
+const App = () => (
+    <div>
+
+        <SignUp/>
+    </div>
+);
+
+export default App;
