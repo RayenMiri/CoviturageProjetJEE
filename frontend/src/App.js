e7e4fe83022e16bf568ad3142e35c27ac7af5c92
Abdellatif Ranim <ranimabdellatif5@gmail.com>
2024-12-01 13:14:26 +0100

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import SignIn from "./Auth/sign_in";
 import SignUp from "./Auth/sign_up";
-
+import Navbar from "./components/Navbar"
 
 const App = () => (
     <div>
-
+        <Navbar />
         <SignUp/>
     </div>
 );
