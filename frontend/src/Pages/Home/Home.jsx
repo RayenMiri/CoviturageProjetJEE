e049bfb3601c5268202783e4787365fbbc700b1f
RayenMiri <rayenmiri000@gmail.com>
2024-12-15 13:19:32 +0100

    fixed 0 seats reserved rides with status confirmed of the actual user display

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -73,13 +73,12 @@ const Home = () => {
         setReviewModalVisible(true);
     };
 
-    const submitReview = (rating,isVisible) => {
+    const submitReview = (rating) => {
         const review = {
             rating,
             ride: { idRide: selectedRide.idRide },
             user: { id: userID },
         };
-        setReviewModalVisible(isVisible)
 
         dispatch(createReview(review)).then((response) => {
             if (response.meta.requestStatus === "fulfilled") {
@@ -143,75 +142,84 @@ const Home = () => {
             </div>
 
             <div className="rides-list flex flex-wrap justify-center column-gap-5 row-gap-5">
-                {filteredRides.map((ride) => (
-                    ride.availableSeats > 0 && (
-                        <div
-                            key={ride.idRide}
-                            className="bg-white flex justify-center items-center w-full sm:w-11/12 md:w-6/12 lg:w-4/12 p-3"
-                        >
+                {filteredRides.map((ride) => {
+                    const isUserBooked = reservations.some(
+                        reservation => reservation.ride.idRide === ride.idRide &&
+                            reservation.user.id.toString() === userID &&
+                            reservation.status === "confirmed"
+                    );
+
+                    if (ride.availableSeats > 0 || isUserBooked) {
+                        return (
                             <div
-                                className="flex flex-col items-center justify-between bg-white dark:bg-gray-800 shadow-md text-gray-800 border border-gray-200 w-full h-auto rounded-xl"
+                                key={ride.idRide}
+                                className="bg-white flex justify-center items-center w-full sm:w-11/12 md:w-6/12 lg:w-4/12 p-3"
                             >
-                                <div className="w-full flex items-center justify-between">
-                                    <div className="flex items-center px-6">
-                                        <img
-                                            className="w-16 rounded-full ring-2 ring-green-600 shadow-lg m-2"
-                                            src='https://img.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg?size=626&ext=jpg'
-                                            alt="User Avatar"
-                                        />
-                                        <div className="text-left">
-                                            <h4 className="text-lg uppercase text-red-500">{ride.title}</h4>
-                                            <h4 className="text-sm font-medium dark:text-gray-200">
-                                                {ride.departureLocation} to {ride.destination}
-                                            </h4>
+                                <div
+                                    className="flex flex-col items-center justify-between bg-white dark:bg-gray-800 shadow-md text-gray-800 border border-gray-200 w-full h-auto rounded-xl"
+                                >
+                                    <div className="w-full flex items-center justify-between">
+                                        <div className="flex items-center px-6">
+                                            <img
+                                                className="w-16 rounded-full ring-2 ring-green-600 shadow-lg m-2"
+                                                src='https://img.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg?size=626&ext=jpg'
+                                                alt="User Avatar"
+                                            />
+                                            <div className="text-left">
+                                                <h4 className="text-lg uppercase text-red-500">{ride.title}</h4>
+                                                <h4 className="text-sm font-medium dark:text-gray-200">
+                                                    {ride.departureLocation} to {ride.destination}
+                                                </h4>
+                                            </div>
+                                        </div>
+                                        <div className="text-right p-2">
+                                            <p className="text-sm text-gray-800 dark:text-gray-200">{ride.date}</p>
                                         </div>
                                     </div>
-                                    <div className="text-right p-2">
-                                        <p className="text-sm text-gray-800 dark:text-gray-200">{ride.date}</p>
-                                    </div>
-                                </div>
-                                <div className="text-left p-2">
-                                    <h3 className="text-base text-green-400 font-semibold">Details</h3>
-                                    <ul className="text-sm list-disc">
-                                        <li><span className="font-bold">Departure Location:</span> {ride.departureLocation}</li>
-                                        <li><span className="font-bold">Destination:</span> {ride.destination}</li>
-                                        <li><span className="font-bold">Available Seats:</span> {ride.availableSeats}</li>
-                                        <li><span className="font-bold">Price Per Seat:</span> {ride.pricePerSeat}</li>
-                                        <li><span className="font-bold">Restrictions:</span> {ride.restrictions}</li>
-                                    </ul>
-                                    <div className="flex justify-end space-x-4">
-                                        {isBooked(ride.idRide) ? (
-                                            <>
+                                    <div className="text-left p-2">
+                                        <h3 className="text-base text-green-400 font-semibold">Details</h3>
+                                        <ul className="text-sm list-disc">
+                                            <li><span className="font-bold">Departure Location:</span> {ride.departureLocation}</li>
+                                            <li><span className="font-bold">Destination:</span> {ride.destination}</li>
+                                            <li><span className="font-bold">Available Seats:</span> {ride.availableSeats}</li>
+                                            <li><span className="font-bold">Price Per Seat:</span> {ride.pricePerSeat}</li>
+                                            <li><span className="font-bold">Restrictions:</span> {ride.restrictions}</li>
+                                        </ul>
+                                        <div className="flex justify-end space-x-4">
+                                            {isUserBooked ? (
+                                                <>
+                                                    <button
+                                                        onClick={() => handleReviewClick(ride)}
+                                                        className="bg-yellow-500 text-white rounded-md px-4 py-2"
+                                                    >
+                                                        Review
+                                                    </button>
+                                                    <button
+                                                        onClick={() => handleCancelClick(ride)}
+                                                        className="bg-red-600 text-white rounded-md px-4 py-2"
+                                                    >
+                                                        Cancel
+                                                    </button>
+                                                </>
+                                            ) : (
                                                 <button
-                                                    onClick={() => handleReviewClick(ride)}
-                                                    className="bg-yellow-500 text-white rounded-md px-4 py-2"
+                                                    onClick={() => handleReserveClick(ride)}
+                                                    className="bg-indigo-600 text-white rounded-md px-4 py-2"
                                                 >
-                                                    Review
+                                                    Reserve
                                                 </button>
-                                                <button
-                                                    onClick={() => handleCancelClick(ride)}
-                                                    className="bg-red-600 text-white rounded-md px-4 py-2"
-                                                >
-                                                    Cancel
-                                                </button>
-                                            </>
-                                        ) : (
-                                            <button
-                                                onClick={() => handleReserveClick(ride)}
-                                                className="bg-indigo-600 text-white rounded-md px-4 py-2"
-                                            >
-                                                Reserve
+                                            )}
+                                            <button className="bg-green-600 text-white rounded-md px-4 py-2">
+                                                <Link to="/ridesMap" className="text-white no-underline">View in Map</Link>
                                             </button>
-                                        )}
-                                        <button className="bg-green-600 text-white rounded-md px-4 py-2">
-                                            <Link to="/ridesMap" className="text-white no-underline">View in Map</Link>
-                                        </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                ))}
+                        );
+                    }
+                    return null;
+                })}
             </div>
 
             {isModalVisible && (
